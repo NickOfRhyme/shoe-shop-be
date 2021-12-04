8bdd892399c0da94ec1cf1846f6b23b38d8a9a3c
NickOfRhyme <50319504+NickOfRhyme@users.noreply.github.com>
2021-12-04 16:58:59 +0000

    basic user endpoint set up

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,21 @@
-const apiRouter = require("express").Router();
+const express = require("express");
+
+const apiRouter = express.Router();
+const usersRouter = express.Router();
+
+apiRouter.use("/users", usersRouter);
 
 apiRouter.route("/").get((req, res, next) => {
   res.send("Golden Shoe API is live!");
+});
+
+usersRouter.route("/").get((req, res, next) => {
+  res.send("Listing users...!");
+});
+
+usersRouter.route("/:user_id").get((req, res, next) => {
+  const { user_id } = req.params;
+  res.send(`Checkout user ${req.params.user_id}`);
 });
 
 module.exports = apiRouter;
