636b116662f34a1f91252755f104be3415647a8b
NickOfRhyme <50319504+NickOfRhyme@users.noreply.github.com>
2021-12-05 23:11:44 +0000

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const apiRouter = require("./routes/routes");
 const app = express();
-const PORT = 3000;
+const cors = require("cors");
+const PORT = 3030;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(apiRouter);
 
